1bf21a9f196f430302b8b27d348d943a9f582af9
Gamepro5 <gamepro543@gmail.com>
2023-01-30 01:57:59 -0800

    Lab1 Done?

--- Lab01/jcrespo7.cpp
+++ Lab01/jcrespo7.cpp
@@ -3,11 +3,37 @@
 
 using namespace std;
 
-int main()
-{
-    string s;
-    cout << "Please enter your first name followed by a newline\n";
-    cin >> s;
-    cout << "Hello, " << s << '\n';
-    return 0; // This return statement isn't necessary
+int main(int argc, char *argv[]) {
+
+    int sortLength = 1;
+    cout << "Sorted array:" << endl;
+    cin >> sortLength;
+    int sort[sortLength];
+
+    for (int i=0;i<sortLength;i++) {
+        cin >> sort[i];
+        //cout << "Added " << sort[i] << " to be sorted.\n";
+    };
+
+    for (int i=1; i<sortLength; i++) {
+        int j = i;
+        while (j > 0 && sort[j-1] > sort[j]) {
+            int temp = sort[j];
+            sort[j] = sort[j-1];
+            sort[j-1] = temp;
+            j -= 1;
+        }
+        for (int k=0;k<i+1;k++) {
+            cout << sort[k] << ";";
+        }
+        cout << endl;
+
+    }
+    cout << endl;
+
+    //to run:  cat test.txt | ./a.exe
+
+    
+
+    return 0;
 }
