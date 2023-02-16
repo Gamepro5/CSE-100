ae8a17ee0a00c4ed74fd3df81a6d9bedb46907b3
Gamepro5 <gamepro543@gmail.com>
2023-02-16 04:45:05 -0800

    Lab3

--- Lab03/jcrespo7.cpp
+++ Lab03/jcrespo7.cpp
@@ -1,80 +1,83 @@
 #include <iostream>
 #include <string>
+#include <limits.h>
 
 using namespace std;
 
-void merge(int arr[], int left, int middle, int right) {
-    int leftArrayLength = middle - left + 1;
-    int rightArrayLength = right - middle;
-    int* leftArray = new int[leftArrayLength];
-    int* rightArray = new int[rightArrayLength];
 
-    for (int i=0;i<leftArrayLength;i++) { //copy data into the two subarrays
-        leftArray[i] = arr[left + i];
-    }
-    for (int j=0;j<rightArrayLength;j++) {
-        rightArray[j] = arr[middle + j + 1];
-    }
+struct findMaxSubarrayResultType {
+    int low;
+    int high;
+    int sum;
+};
 
-    int leftArrayIndex=0, rightArrayIndex=0;
-
-    int mergedArrayIndex = left;
-
-    while (leftArrayIndex < leftArrayLength && rightArrayIndex < rightArrayLength) { //merging
-        if (leftArray[leftArrayIndex] <= rightArray[rightArrayIndex]) { //rebuilds the original array by comparing th first element of each subarray
-            arr[mergedArrayIndex] = leftArray[leftArrayIndex];
-            leftArrayIndex++;
-        } else {
-            arr[mergedArrayIndex] = rightArray[rightArrayIndex];
-            rightArrayIndex++;
+findMaxSubarrayResultType findMaxCrossingSubarray(int* arr, int low, int mid, int high) {
+    findMaxSubarrayResultType result;
+    int leftSum = INT_MIN;
+    int sum = 0;
+    for (int i=mid;i>low;i--) {
+        sum += arr[i];
+        if (sum > leftSum) {
+            leftSum = sum;
+            result.low = i; // maxLeft
         }
-        mergedArrayIndex++;
     }
-
-    //copy them back into original array
-    while (leftArrayIndex < leftArrayLength) {
-        arr[mergedArrayIndex] = leftArray[leftArrayIndex];
-        leftArrayIndex++;
-        mergedArrayIndex++;
+    int rightSum = INT_MIN;
+    sum = 0;
+    for (int i=mid+1; i<high; i++) {
+        sum += arr[i];
+        if (sum > rightSum) {
+            rightSum = sum;
+            result.high = i; // maxRight
+        }
     }
-    while (rightArrayIndex < rightArrayLength) {
-        arr[mergedArrayIndex] = rightArray[rightArrayIndex];
-        rightArrayIndex++;
-        mergedArrayIndex++;
-    }
-    delete[] leftArray;
-    delete[] rightArray;
-    
+    result.sum = leftSum + rightSum;
+    return result;
 }
 
-void mergeSort(int arr[], int left, int right) {
-    if (left >= right) {
-        return;
-    }
-    int middle = left + ((right-left)/2);
-    mergeSort(arr, left, middle);
-    mergeSort(arr, middle+1, right);
-    merge(arr, left, middle, right);
-}
 
+findMaxSubarrayResultType findMaxSubarray(int *arr, int low, int high) {
+    if (high == low) {
+        findMaxSubarrayResultType result;
+        result.low = low;
+        result.high = high;
+        result.sum = arr[low];
+        return result;
+    } else {
+        int mid = (low+high)/2;
+        findMaxSubarrayResultType left = findMaxSubarray(arr, low, mid);
+        findMaxSubarrayResultType right = findMaxSubarray(arr, mid+1, high);
+        findMaxSubarrayResultType cross = findMaxCrossingSubarray(arr, low, mid, high);
+        if (right.sum >= left.sum && left.sum >= cross.sum) {
+            return left;
+        } else if (right.sum >= left.sum && right.sum >= cross.sum) {
+            return right;
+        } else {
+            return cross; 
+        }
+    }
+}
 
 int main(int argc, char *argv[]) {
 
-    int sortLength = 1;
+    int arrLength = 1;
     //cout << "Sorted array:" << endl;
-    cin >> sortLength;
-    int sort[sortLength];
+    cin >> arrLength;
+    int arr[arrLength];
 
-    for (int i=0;i<sortLength;i++) {
-        cin >> sort[i];
+    for (int i=0;i<arrLength;i++) {
+        cin >> arr[i];
         //cout << "Added " << sort[i] << " to be sorted.\n";
     };
 
-    mergeSort(sort, 0, sortLength-1);
+    findMaxSubarrayResultType result = findMaxSubarray(arr, 0, arrLength);
 
-    for (int i=0;i<sortLength;i++) {
-        cout << sort[i] << ";";
-    }
+    //int sum = 0;
+    //for (int i=result.low;i<result.high;i++) {
+    //   std::cout << arr[i] << ";";
+    //   sum += arr[i];
+    //}
+    std::cout << result.sum;
     //cout << endl;
 
     //to run:  cat test.txt | ./a.exe
