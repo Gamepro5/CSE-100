701422c9316be76dfeaaca7f0fe61dbfae87ca6a
Gamepro5 <gamepro543@gmail.com>
2023-04-07 14:35:10 -0700

    lab 9 progress?

--- Lab09/jcrespo7.cpp
+++ Lab09/jcrespo7.cpp
@@ -4,69 +4,55 @@
 
 using namespace std;
 
-void swap(int** arr, int q, int r) {
-    int temp = (*arr)[q];
-    (*arr)[q] = (*arr)[r];
-    (*arr)[r] = temp;
-}
 
-int partition(int* arr, int p, int r) {
-    int x =  arr[r];
-    int i = p-1;
-    for (int j=p;j<=r-1;j++) {
-        if (arr[j] <= x) {
-            i = i+1;
-            swap(&arr, i, j);
-        }
+void minHeapify(node* arr, int n, int i) {
+    int max = i;
+    int leftChild = (2*i) + 1;
+    int rightChild = (2*i) + 2;
+    if ((leftChild < n) && (arr[leftChild] > arr[max])) {
+        max = leftChild;
     }
-    swap(&arr, i+1, r);
-    return i+1;
-}
-
-int randomizedPartition(int* arr, int p, int r) {
-    srand((unsigned) time(NULL));
-    int i = (rand() % (r - p) +p);//random number from p to r
-    swap(&arr, r, i);
-    return partition(arr, p, r);
-}
-
-int randomizedSelect(int* arr, int p, int r, int i) {
-    if (p==r) {
-        return arr[p];
+    if ((rightChild < n) && (arr[rightChild] > arr[max])) {
+        max = rightChild;
     }
-    int q = partition(arr, p, r);
-    cout << arr[q] << ";";
-    int k = q - p + 1;
-    if (i==k) { //the pivot value is the answer
-        return arr[q];
-    } else if (i<k) {
-        return randomizedSelect(arr, p, q-1, i);
-    } else {
-        return randomizedSelect(arr, q+1, r, i-k);
+    if (max != i) {
+        int temp = arr[i];
+        arr[i] = arr[max];
+        arr[max] = temp;
+        minHeapify(arr, n, max);
     }
 }
 
+void buildMinHeap(node* arr) {
+    for(int i=n/2 -1;i>=0;i--){
+       minHeapify(arr,n,i);
+    }
+}
 
 
 int main(int argc, char *argv[]) {
 
     int arrLength = 1;
-    int nth_search;
     //cout << "Sorted array:" << endl;
     cin >> arrLength;
-    
     int arr[arrLength];
-
-    cin >> nth_search;
 
     for (int i=0;i<arrLength;i++) {
         cin >> arr[i];
         //cout << "Added " << sort[i] << " to be sorted.\n";
     };
-    //int n = sizeof(arr) / sizeof(arr[0]);
+    int n = sizeof(arr) / sizeof(arr[0]);
+    for(int i=n/2 -1;i>=0;i--){
+       heapify(arr,n,i);
+    }
     
-    cout << randomizedSelect(arr,0,arrLength-1, nth_search) << ";";
+    heapSort(arr, arrLength);
 
+    //int sum = 0;
+    for (int i=0;i<arrLength;i++) {
+       std::cout << arr[i] << ";";
+       //sum += arr[i];
+    }
     //cout << endl;
 
     //to run:  cat test.txt | ./a.exe
